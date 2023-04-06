ef9ff9ea5b596189da19c9834d829d3871c27370
liumengdie <liumd@getui.com>
2023-04-07 03:02:58 +0800

    feat:完成布局

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,96 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
-<template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+<!-- <template>
+  <Todolist />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+
+<script  >
+import { reactive } from 'vue';
+import Todolist from './components/Todolist.vue';
+
+
+export default{
+  components:{
+    Todolist
+  },
+  setup(){
+    const state=reactive({
+      todos:[
+        {id:1,content:'bihao',isCompleted:false},
+        {id:2,content:'bihao',isCompleted:false},
+        {id:3,content:'bihao',isCompleted:false}
+
+
+      ]
+    })
+    return{
+
+    }
+  }
+  
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+</script>
+
+  
+<style></style> -->
+
+<template>
+<el-card class="box-card">
+    <template #header>
+      <div class="card-header">
+        <el-input v-model="input" placeholder="请输入待办事项" />
+      </div>
+    </template>
+    <div v-for="o in 4" :key="o" class="text item">{{ 'List item ' + o }}</div>
+
+    <div class="text">
+       <el-row :gutter="15" >
+            <el-col :span="8"><div class="grid-content ep-bg-purple" />共三件事情</el-col>
+            <el-col :span="3"><div class="grid-content ep-bg-purple" /><el-button text>全部</el-button></el-col>
+            <el-col :span="4"><div class="grid-content ep-bg-purple" /><el-button text>进行中</el-button></el-col>
+            <el-col :span="4"><div class="grid-content ep-bg-purple" /><el-button text>已完成</el-button></el-col>
+         </el-row>
+    </div>
+  </el-card>
+</template>
+
+
+<script setup>
+import { ref } from 'vue'
+const input = ref('')
+
+</script>
+
+  
+<style>
+.el-row {
+  margin-bottom: 20px;
+  
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.el-row:last-child {
+  margin-bottom: 0;
+}
+.el-col {
+  border-radius: 4px;
+}
+
+.card-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.text {
+  font-size: 14px;
+}
+
+
+.item {
+  margin-bottom: 18px;
+}
+
+.box-card {
+  margin: 50px auto;
+  width: 600px;
 }
 </style>
